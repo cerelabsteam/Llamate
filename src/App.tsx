f0b0dddbc58d1163c73efcae7e09844a63444c6b
parth-mangtani-cerelabs <parth.mangtani@cerelabs.com>
2024-06-05 15:14:53 +0530

    ➕ add mui/material

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./stylesheets/App.css";
 
+import { Button } from "@mui/material";
+
 function App() {
   return (
     <>
       <h1>llamate</h1>
+      <Button>Start</Button>
     </>
   );
 }
