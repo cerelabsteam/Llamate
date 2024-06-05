f04dc33ca1127a641e22add160e3c6b51a1b5676
Amish Palkar <amish.palkar.cerelabs@gmail.com>
2024-06-05 17:52:24 +0530

    🚧 Added  components in llamate sidebar

--- src/PromptEngineeringSidebar.tsx
+++ src/PromptEngineeringSidebar.tsx
@@ -1,20 +1,36 @@
 import "./stylesheets/PromptEngineeringSidebar.css";
 
 import { Dispatch, SetStateAction } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-import InboxIcon from "@mui/icons-material/Inbox";
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
   Drawer,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
 } from "@mui/material";
+import React from "react";
 
 function PromptEngineeringSidebar(props: {
   isPromptEngineeringSidebarOpen: boolean;
   changeIsPromptEngineeringSidebarOpen: Dispatch<SetStateAction<boolean>>;
 }) {
+  // state
+  const [template, changeTemplate] = React.useState("");
+
+  const handleChangeTemplate = (event: SelectChangeEvent) => {
+    changeTemplate(event.target.value as string);
+  };
+
   return (
     <Drawer
       open={props.isPromptEngineeringSidebarOpen}
@@ -22,14 +38,47 @@ function PromptEngineeringSidebar(props: {
         props.changeIsPromptEngineeringSidebarOpen(false);
       }}
     >
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Inbox" />
-        </ListItemButton>
-      </ListItem>
+      <Typography variant="h6">Setup</Typography>
+      <form>
+        <Button variant="outlined">Apply changes</Button>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={template}
+            label="Age"
+            onChange={handleChangeTemplate}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          id="outlined-multiline-static"
+          label="System message"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+        <Button variant="outlined" startIcon={<AddIcon />}>
+          Add example
+        </Button>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            Example 1
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+      </form>
     </Drawer>
   );
 }
