b15518400a78b3615f7fcf4cb0ba3f80a98e4544
parth-mangtani-cerelabs <parth.mangtani@cerelabs.com>
2024-06-08 13:54:49 +0530

    🚸 textfields in example accordians are multiline now

--- src/PromptEngineeringSidebar.tsx
+++ src/PromptEngineeringSidebar.tsx
@@ -195,6 +195,8 @@ function PromptEngineeringSidebar(props: {
               <AccordionDetails className="PromptEngineeringSidebar-ExampleAccordionDetails">
                 <TextField
                   value={example.user}
+                  multiline
+                  rows={4}
                   label="User:"
                   onChange={(e) => {
                     handleChangeExampleTextField(e.target.value, idx, "user");
@@ -202,6 +204,8 @@ function PromptEngineeringSidebar(props: {
                 />
                 <TextField
                   value={example.assistant}
+                  multiline
+                  rows={4}
                   label="Assistant:"
                   onChange={(e) => {
                     handleChangeExampleTextField(
