42f175c03c36d51ade1abdb415b8b4c54eee8909
Gauri Sharma <gauri.sharma.cerelabs@gmail.com>
2024-07-02 11:04:51 +0530

    ✨

---
+++ src/types/PdfCovert.d.ts
@@ -0,0 +1,9 @@
+declare module "pdfmake/build/pdfmake" {
+  const pdfMake: any;
+  export default pdfMake;
+}
+
+declare module "pdfmake/build/vfs_fonts" {
+  export const pdfMake: any;
+  export const vfs: any;
+}
