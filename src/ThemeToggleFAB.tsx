2854744db258491989b710e89dc8eb04c92070b1
Gauri Sharma <gauri.sharma.cerelabs@gmail.com>
2024-07-12 13:02:49 +0530

    💄

--- src/ThemeToggleFAB.tsx
+++ src/ThemeToggleFAB.tsx
@@ -5,12 +5,18 @@ import LightModeIcon from "@mui/icons-material/LightMode";
 import { IconButton } from "@mui/material";
 
 import type { PaletteOptions } from "@mui/material/styles";
+
 function ThemeToggleFAB(props: {
   handleThemeToggle: () => void;
   currentThemePalette: PaletteOptions["mode"];
 }) {
+  const themeClass = props.currentThemePalette === "light" ? "light" : "dark";
+
   return (
-    <IconButton onClick={props.handleThemeToggle} className="ThemeToggleFAB">
+    <IconButton
+      onClick={props.handleThemeToggle}
+      className={`ThemeToggleFAB ${themeClass}`}
+    >
       {props.currentThemePalette === "light" ? (
         <DarkModeIcon />
       ) : (
